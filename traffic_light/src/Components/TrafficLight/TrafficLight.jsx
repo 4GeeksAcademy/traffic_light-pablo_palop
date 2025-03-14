8a932579b982b9ddcc6fad59b24d63c584c254de
pablopalop <130085149+pablopalop@users.noreply.github.com>
2025-03-14 18:03:10 +0000

    Purple añadido y funcionando

--- traffic_light/src/Components/TrafficLight/TrafficLight.jsx
+++ traffic_light/src/Components/TrafficLight/TrafficLight.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 
 function TrafficLight() {
-  const [light, setLight] = useState(null)
-  const [isAuto, setIsAuto] = useState(false)
+  const [light, setLight] = useState(null);
+  const [isAuto, setIsAuto] = useState(false);
+  const [purple, setPurple] = useState(false);
 
   const changeLight = (color) => {
     setLight(color);
@@ -10,26 +11,31 @@ function TrafficLight() {
 
   useEffect(() => {
     if (!isAuto) return;
-    
-    const lights = ["red", "yellow", "green"];
+
+    const lights = purple ? ["red", "yellow", "green", "purple"] : ["red", "yellow", "green"];
     let index = 0;
-    
+
     const interval = setInterval(() => {
-      setActiveLight(lights[index]);
+      setLight(lights[index]);
       index = (index + 1) % lights.length;
     }, 3000);
-    
+
     return () => clearInterval(interval);
-  }, [isAuto]);
+  }, [isAuto, purple]);
 
   const autoChange = () => {
     setIsAuto(!isAuto);
   };
+  const showPurple = () => {
+    setPurple(!purple);
+  };
+  
+
 
   return (
     <div className="flex flex-col items-center m-5">
       <div className="w-2 h-12 bg-black"></div>
-      <div className="w-20 h-52 bg-black rounded-lg flex flex-col justify-around items-center p-2 shadow-xl">
+      <div className="w-20 h-auto bg-black rounded-lg flex flex-col justify-around items-center p-2 shadow-xl space-y-3">
         <div
           className={`w-14 h-14 bg-red-600 rounded-full cursor-pointer transition-all duration-300 ${light === 'red' ? 'drop-shadow-[0_0_60px_rgb(255,0,0)]' : ''}`}
           onClick={() => changeLight('red')}
@@ -42,9 +48,18 @@ function TrafficLight() {
           className={`w-14 h-14 bg-green-600 rounded-full cursor-pointer transition-all duration-300 ${light === 'green' ? 'drop-shadow-[0_0_60px_rgb(0,255,0)]' : ''}`}
           onClick={() => changeLight('green')}
         ></div>
+        {purple && (
+          <div
+            className={`w-14 h-14 bg-purple-600 rounded-full cursor-pointer transition-all duration-300 ${light === 'purple' ? 'drop-shadow-[0_0_60px_rgb(130,0,255)]' : ''}`}
+            onClick={() => changeLight('purple')}
+          ></div>
+        )}
       </div>
       <button type="button" className="btn btn-primary mt-3" onClick={autoChange}>
         {isAuto ? "Stop Traffic Light" : "Activate Traffic Light"}
+      </button>
+      <button type="button" className="btn btn-warning mt-3" onClick={showPurple}>
+        {purple ? "Hide purple light" : "Show purple light"}
       </button>
     </div>
   );
